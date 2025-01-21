cf3c94372e95015cfcb5b68fd436dcf6de17f1bd
Grace <desert.jiang@gmail.com>
2025-01-21 16:21:21 -0500

    Fix: resolve pagination link navigation issue

--- src/component/Pagination.jsx
+++ src/component/Pagination.jsx
@@ -4,13 +4,13 @@ const Pagination = ({pagination, getProducts}) =>{
     <>
       <nav aria-label="Page navigation">
         <ul className="pagination pagination-sm justify-content-end">
-          <li className={`page-item ${has_pre ? "" : "disabled"}`}><a className="page-link" href="/" onClick={() => getProducts(current_page - 1)}>Previous</a></li>
+          <li className={`page-item ${has_pre ? "" : "disabled"}`}><a className="page-link" href="#" onClick={() => getProducts(current_page - 1)}>Previous</a></li>
           {[...new Array(total_pages)].map((page, index) => {
             return (
-              <li className ={`page-item ${current_page === index + 1 ? "active" : ""}`} aria-current="page" key={index + 1}><a className="page-link" href="/" onClick={() => getProducts(index + 1)}>{index + 1}</a></li>
+              <li className ={`page-item ${current_page === index + 1 ? "active" : ""}`} aria-current="page" key={index + 1}><a className="page-link" href="#" onClick={() => getProducts(index + 1)}>{index + 1}</a></li>
             )
           })}
-          <li className={`page-item ${has_next ? "" : "disabled"}`}><a className="page-link" href="/" onClick={() => getProducts(current_page + 1)}>Next</a></li>
+          <li className={`page-item ${has_next ? "" : "disabled"}`}><a className="page-link" href="#" onClick={() => getProducts(current_page + 1)}>Next</a></li>
         </ul>
       </nav>
     </>
